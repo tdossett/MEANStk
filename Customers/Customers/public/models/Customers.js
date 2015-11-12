8337e374c7b0480aa4c659f0e6131e67d3a37c08
tim dossett <tdossett@pacbell.net>
2015-11-12 12:57:19 -0800

    Administration Customer - changes

---
+++ Customers/Customers/public/models/Customers.js
@@ -0,0 +1,11 @@
+﻿var mongoose = require('mongoose');
+
+var CustomerSchema = new mongoose.Schema({
+    Id: String,
+    firstname: String,
+    lastname: String,
+    email: String,
+    password: String
+});
+
+mongoose.model('Customer', CustomerSchema);
